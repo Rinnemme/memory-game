f8c4b84171e689645aa3d09bd2bea2544e3c6192
Emmett Herrick <123272376+Rinnemme@users.noreply.github.com>
2024-01-10 11:49:58 -0500

    Clarify rules to imply the max score of 100 weapons, and tweak game over message to include 'don't give up, skeleton!' as an ER inside joke

--- src/GameOver.jsx
+++ src/GameOver.jsx
@@ -2,8 +2,11 @@ function GameOver({score, replay}) {
     return (
         <div className="modal">
             <div className="modal-window">
-            {score===100 && <p>{`Well done, Tarnished! You got all 100 weapons in your arsenal. I guess all that's left now, is to try to do it again!`}</p>}
-            {score!==100 && <p>{`You accumulated ${score} weapons before Miyazaki confiscated your aresenal and banished you from the Lands Between.`}</p>}
+            {score===100 && <p>{`Well done, Tarnished! You got all 100 weapons in your arsenal. 
+                                I guess all that's left now, is to try to do it again!`}</p>}
+            {score!==100 && <p>{`You accumulated ${score} weapons before Miyazaki confiscated 
+                                your aresenal and banished you from the Lands Between. Don't 
+                                give up, skeleton!`}</p>}
             <button onClick={() => replay()}>OK</button>
             </div>
         </div>
--- src/Rules.jsx
+++ src/Rules.jsx
@@ -2,10 +2,12 @@ function Rules({closeRules}) {
     return (
         <div className="modal">
             <div className="modal-window">
-            <p>Five weapons at a time will appear on your screen, and you must 
-                click a weapon to add it to your arsenal. However, if you try 
-                to add the same weapon twice, Miyazaki will punish your greed 
-                by emptying your arsenal entirely. Good luck, Tarnished!</p>
+            <p>Five weapons at a time will appear on your screen from a list 
+                of 100 Elden Ring Weapons, and you must click a weapon to add 
+                it to your arsenal. However, if you try to add the same weapon 
+                twice, Miyazaki will punish your greed by emptying your arsenal 
+                entirely and banishing you from the Land Between. Good luck, 
+                Tarnished!</p>
             <button onClick={() => closeRules()}>OK</button>
             </div>
         </div>
