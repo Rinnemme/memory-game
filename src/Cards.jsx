f6986ef51b26d828ace15e637fc046d3ea40071c
Emmett Herrick <123272376+Rinnemme@users.noreply.github.com>
2024-01-10 10:57:39 -0500

    change Cards to never display the same card in the same spot more than once, and add an unclickedIDs array for use in ensuring there's always an unclicked card when cards render

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -3,7 +3,9 @@ import {useEffect} from 'react'
 
 function Cards ({array, incrementScore, gameOver}) {
     const [indexes, setIndexes] = useState([])
+    const [lastIndexes, setLastIndexes] = useState([])
     const [clickedIDs, setClickedIDs] = useState([])
+    const unclickedIDs = array.map(weapon => weapon.id).filter(id => !clickedIDs.includes(id))
 
     function resetClickedIDs() {
         setClickedIDs([])
@@ -17,24 +19,30 @@ function Cards ({array, incrementScore, gameOver}) {
         if (clickedIDs.includes(id)) {
             resetClickedIDs()
             gameOver()
+        } else if (clickedIDs.length===99) {
+            incrementScore()
+            gameOver()
         } else {
             addClickedID(id)
+            const newUnclickedIDs = unclickedIDs.filter(number => number!==id)
+            console.log(`as of clicking this, there are ${unclickedIDs.length} unclicked IDs and ${clickedIDs.length} clicked IDs`) 
             incrementScore()
     }}
 
     useEffect(() => {
-        displayRandomCards()
+        displayNewCards()
     }, [])
     
-    function displayRandomCards() {
+    function displayNewCards() {
         const newIndexes = []
         while (newIndexes.length < 5) {
             const number = Math.floor(Math.random()*array.length)
-            if (!newIndexes.includes(number)) {
+            if (!newIndexes.includes(number) && lastIndexes[newIndexes.length]!==number) {
                 newIndexes.push(number)
                 }
             }
         setIndexes(newIndexes)
+        setLastIndexes(newIndexes)
     }
 
     return (
@@ -42,7 +50,7 @@ function Cards ({array, incrementScore, gameOver}) {
             {indexes.length>0 && indexes.map((index) => {
                 return <div key={array[index].id} className="card" onClick = {() => {
                     processID(array[index].id)
-                    displayRandomCards()
+                    displayNewCards()
                 }}>
                 <p>{array[index].name}</p>
                 <img src = {array[index].image}></img>
